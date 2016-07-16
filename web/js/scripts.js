be70eae3797907c028df2d1aa0d85b4a4456c746
K.Wizner <crisw2012@gmail.com>
2016-07-16 21:14:16 +0200

    changing everything

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -1,13 +1,3 @@
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    };
-    xmlHttp.open("GET", theUrl, true);
-    xmlHttp.send(null);
-}
 
 function callbackPrint(response){
 	console.log(response);
